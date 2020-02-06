56da418b61269fcb32144944e232d435d8e668b8
dsmirnov <dsmirnov@mmtr.ru>
2020-02-06 12:15:31 +0300

    2020_02_06 first commit start project

---
+++ UI/app.js
@@ -0,0 +1,108 @@
+'use strict';
+var XML, ipAdress;
+var serverUrlIndex = 1;
+loadXML('client.config.xml');
+function parseXML() {
+    ipAdress = XML.getElementsByTagName('server')[serverUrlIndex].getAttribute('url');
+};
+function loadXML(url) {
+    var oXML;
+    if (window.XMLHttpRequest) {
+        oXML = new XMLHttpRequest();
+    } else if (window.ActiveXObject) {
+        oXML=new ActiveXObject("Microsoft.XMLHTTP");
+    };
+
+    if (oXML) {
+        oXML.onreadystatechange = function() {
+            if (oXML.readyState == 4) {
+                if (oXML.status == 200) {
+                    XML = oXML.responseXML;
+                    parseXML();
+                };
+            };
+        };
+        oXML.open("GET", url, true);
+        oXML.send();
+    };
+};
+
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function() {
+
+    var Auth={
+        authorize: function(userService) {
+            return userService.getNewUser();
+        }
+    };
+
+   $routeProvider
+       .otherwise({redirectTo: '/loginPage'})
+       .when('/loginPage', {
+           templateUrl: 'loginPage/loginPage.html',
+           controller: 'LoginPageController'
+       })
+       .when('/users', {
+           templateUrl: 'users/users.html',
+           controller: 'AbiturientCtrl'
+       })
+});
+
+myApp.controller('CopyrightDateCtrl',function ($scope, dateFilter) {
+    $scope.date = new Date();
+    $scope.$watch('date', function (date)
+    {
+        $scope.dateString = dateFilter(date, 'yyyy');
+    });
+});
+
+
+
+myApp.controller('UserCtrl', function($scope) { //это контроллер , он ставится в шаблоне html ng-controller="UserCtrl" - и отвечает за видимость внутри вложенных dom элементов старницы
+
+    //$scope видимость контекста внутри контроллера
+    $scope.helloWorld = "hello world";
+
+    function getText(){ //функция js
+        var text = 1;
+        text = text + 1;
+        return text;
+    }
+
+    $scope.getText = function(){ //функция ангуляра она может быть вызвана например событиями ng-click="getText()" или ng-mouseenter и другие
+        var text = 1;
+        text = text + 1;
+        return text;
+    }
+
+});
+
+
+
+
+
+
+myApp.filter('notNull', function(){
+    return function(input){
+        if (input && input.search('null')!=-1)
+        {
+            input = input.replace('null', '');
+        }
+        return input;
+    }
+});
+
+//Функция смены курсора при наведении на правый нижний угол элемента textarea
+$(function() {
+    //  changes mouse cursor when highlighting loawer right of box
+    $(document).on('mousemove', 'textarea', function (e) {
+        var a = $(this).offset().top + $(this).outerHeight() - 16,	//	top border of bottom-right-corner-box area
+            b = $(this).offset().left + $(this).outerWidth() - 16;	//	left border of bottom-right-corner-box area
+        $(this).css({
+            cursor: e.pageY > a && e.pageX > b ? 'nw-resize' : ''
+        });
+    })
+});
+
+
---
+++ UI/services.js
@@ -0,0 +1,285 @@
+
+var services = angular.module('myApp.services', ['ngRoute']);
+
+services.factory('datesService', function () {
+    var service = {};
+
+    service.getSmallMonth = function(date) {
+        switch (date.getMonth()) {
+            case 0:
+                return "янв";
+            case 1:
+                return "фев";
+            case 2:
+                return "мар";
+            case 3:
+                return "апр";
+            case 4:
+                return "май";
+            case 5:
+                return "июн";
+            case 6:
+                return "июл";
+            case 7:
+                return "авг";
+            case 8:
+                return "сен";
+            case 9:
+                return "окт";
+            case 10:
+                return "ноя";
+            case 11:
+                return "дек";
+        };
+    };
+
+    service.getShortDayOfWeekName = function(dayNumber) {
+        switch (dayNumber) {
+            case 0:
+                return "Вскр";
+            case 1:
+                return "Пн";
+            case 2:
+                return "Вт";
+            case 3:
+                return "Ср";
+            case 4:
+                return "Чт";
+            case 5:
+                return "Пт";
+            case 6:
+                return "Сб";
+        };
+    };
+
+    service.parseDateToStringSmall = function(date) {
+        var date = new Date(date);
+        var month = date.getMonth()+1;
+        month < 10 ? month = "0" + month : "";
+        var day = date.getDate();
+        day < 10 ? day = "0" + day : "";
+        return day + '.' + month;
+    };
+
+    service.parseDateToStringRussian = function(date) {
+        var date = new Date(date);
+        var month = date.getMonth()+1;
+        month < 10 ? month = "0" + month : "";
+        var day = date.getDate();
+        day < 10 ? day = "0" + day : "";
+        return day + '.' + month + '.' + date.getFullYear();
+    };
+
+    service.parseDateToString = function(date) {
+        var date = new Date(date);
+        var month = date.getMonth()+1;
+        month < 10 ? month = "0" + month : "";
+        var day = date.getDate();
+        day < 10 ? day = "0" + day : "";
+        return date.getFullYear() + '-' + month + '-' + day;
+    };
+
+    service.getDateClass = function(date, productionCalendarList) {
+        if (date.getDate()==new Date().getDate() && date.getMonth()==new Date().getMonth() && date.getYear()==new Date().getYear()) return "info";
+        else {
+            if (productionCalendarList) {
+                var day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+                var month = (date.getMonth() + 1) < 10 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1);
+                var dateFromReport = date.getFullYear() + '-' + month + '-' + day;
+                var dateFromList = productionCalendarList[productionCalendarList.map(function (e){return e.wDate;}).indexOf(dateFromReport)];
+                if (dateFromList) {
+                    if (dateFromList.isDAyOff == true) {
+                        return "danger";
+                    };
+                };
+            } else {
+                var day = date.getDay();
+                if (day==0 || day==6) {
+                    return "danger";
+                };
+            };
+        };
+        return "";
+    };
+
+    service.isWeekendDate = function(date, productionCalendarList) {
+        if (productionCalendarList) {
+            var day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+            var month = (date.getMonth() + 1) < 10 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1);
+            var dateFromReport = date.getFullYear() + '-' + month + '-' + day;
+            var dateFromList = productionCalendarList[productionCalendarList.map(function (e){return e.wDate;}).indexOf(dateFromReport)];
+            if (dateFromList) {
+                if (dateFromList.isDAyOff == true) {
+                    return true;
+                };
+            };
+        } else {
+            var day = date.getDay();
+            if (day==0 || day==6) {
+                return true;
+            };
+        };
+        return false;
+    };
+
+    service.getTimeStringByDate = function(date) {
+        if (date) {
+            var hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
+            var minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+            var seconds = "00";
+            return hours + ":" + minutes;
+        };
+        return null;
+    };
+
+    service.isEqualDates = function(date1, date2) {
+        if (date1, date2) {
+            var d1 = new Date(date1);
+            var d2 = new Date(date2);
+            if (d1.getFullYear()==d2.getFullYear() && d1.getMonth()==d2.getMonth() && d1.getDate()==d2.getDate()) {
+                return true;
+            };
+        };
+        return false;
+    };
+
+    service.isFirstDateBeforeSecondDate = function(date1, date2) {
+        if (date1, date2) {
+            var d1 = new Date(date1);
+            d1.setHours(0);
+            d1.setMinutes(0);
+            d1.setSeconds(0);
+            d1.setMilliseconds(0);
+            var d2 = new Date(date2);
+            d2.setHours(0);
+            d2.setMinutes(0);
+            d2.setSeconds(0);
+            d2.setMilliseconds(0);
+            if (d1.getTime()<d2.getTime()) {
+                return true;
+            };
+        };
+        return false;
+    };
+
+    service.getDateWithoutTime = function(date) {
+        var d = new Date(date);
+        d.setHours(0);
+        d.setMinutes(0);
+        d.setSeconds(0);
+        d.setMilliseconds(0);
+        return d;
+    };
+
+    service.getPrettyRussianDateStringByDate = function (date) {
+        switch (date.getMonth()) {
+            case 0:
+                return date.getDate() + " января " + date.getFullYear();
+            case 1:
+                return date.getDate() + " февраля " + date.getFullYear();
+            case 2:
+                return date.getDate() + " марта " + date.getFullYear();
+            case 3:
+                return date.getDate() + " апреля " + date.getFullYear();
+            case 4:
+                return date.getDate() + " мая " + date.getFullYear();
+            case 5:
+                return date.getDate() + " июня " + date.getFullYear();
+            case 6:
+                return date.getDate() + " июля " + date.getFullYear();
+            case 7:
+                return date.getDate() + " августа " + date.getFullYear();
+            case 8:
+                return date.getDate() + " сентября " + date.getFullYear();
+            case 9:
+                return date.getDate() + " октября " + date.getFullYear();
+            case 10:
+                return date.getDate() + " ноября " + date.getFullYear();
+            case 11:
+                return date.getDate() + " декабря " + date.getFullYear();
+        };
+    };
+
+    return service;
+});
+
+services.filter('orderObjectBy', function() {
+    return function(items, field, reverse) {
+        var filtered = [];
+        var nullObjects = [];
+
+        angular.forEach(items, function(item) {
+            if(item[field]) filtered.push(item);
+            else nullObjects.push(item);
+        });
+
+        filtered.sort(function (a, b) {
+            return (a[field] > b[field] ? 1 : -1);
+        });
+
+        if(reverse) filtered.reverse();
+
+        angular.forEach(nullObjects, function(item) {
+            filtered.push(item);
+        });
+
+        return filtered;
+    };
+});
+
+
+myApp.factory('userService', function($http, $window, $q, $location, $timeout, $rootScope) {
+
+    var service = {};
+    if (!localStorage.token) {
+        service = { User: undefined };
+    };
+
+    service.conf={headers: {}};
+    service.conf.headers.Authorization = "";
+
+    if (localStorage.token)
+        service.conf.headers.Authorization =  localStorage.token;
+    else if (sessionStorage.token)
+        service.conf.headers.Authorization =  sessionStorage.token;
+
+    service.GetUserByToken = function() {
+        var deferred = $q.defer();
+        $http.get(ipAdress + '/ts-rest/employee/user_by_token', service.conf).success(function(user){
+            service.User = testIsActiveUser(user);
+            deferred.resolve(user);
+            if (!service.User) {
+                $location.path('/loginPage');
+            };
+        }).error(function(){
+            service.User = undefined;
+            $location.path('/loginPage');
+        });
+        return deferred.promise;
+    };
+
+    service.Authorize = function(employee) {
+        var deferred = $q.defer();
+        $http.post(ipAdress + '/ts-rest/employee/authorization', employee).success(function (user) {
+            //todo
+        }).error(function (data) {
+            service.User = undefined;
+            delete localStorage.token;
+            delete sessionStorage.token;
+            $location.path('/loginPage');
+            deferred.reject(null);
+        });
+
+        return deferred.promise;
+    };
+
+
+
+    service.LogOut = function() {
+        service.User = undefined;
+        if (localStorage.token) delete localStorage.token;
+        else delete sessionStorage.token;
+    };
+
+    return service;
+});
