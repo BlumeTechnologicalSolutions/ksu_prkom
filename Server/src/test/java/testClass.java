a8e5d9cb2a6b601e288ed029eac8bbe41459c305
dsmirnov <dsmirnov@mmtr.ru>
2020-02-08 14:39:57 +0300

    commit 2020_02_08 №3

---
+++ Server/src/test/java/testClass.java
@@ -0,0 +1,39 @@
+import com.mongodb.*;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+public class testClass {
+
+    public static void main(String[] args ){
+        System.out.println("Start mongodb:");
+        MongoClient mongoClient = new MongoClient("localhost", 27017);
+
+        //MongoClient mongoClient = new MongoClient();
+
+
+        for(String db: mongoClient.getDatabaseNames() ){
+            System.out.println("DB mongodb:"+db);
+        }
+        DB database = mongoClient.getDB("testDb");
+
+        DBCollection collection = database.getCollection("testCol");
+
+        BasicDBObject searchQuery = new BasicDBObject();
+        searchQuery.put("", "");
+        DBCursor cursor = collection.find();
+
+        while (cursor.hasNext()) {
+            System.out.println(cursor.next());
+        }
+        BasicDBObject document = new BasicDBObject();
+        document.put("name", "Shubham");
+        document.put("company", "Baeldung");
+        collection.insert(document);
+
+        cursor = collection.find();
+        while (cursor.hasNext()) {
+            System.out.println(cursor.next());
+        }
+    }
+
+}
