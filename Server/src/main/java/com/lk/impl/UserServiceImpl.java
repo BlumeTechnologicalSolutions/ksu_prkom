f63af7142547729d8082b8079879a0865f0da12e
dsmirnov <dsmirnov@mmtr.ru>
2020-02-10 15:18:57 +0300

    commit 2020_02_10 #2

--- Server/src/main/java/com/lk/impl/UserServiceImpl.java
+++ Server/src/main/java/com/lk/impl/UserServiceImpl.java
@@ -9,8 +9,12 @@ import com.lk.service.UserService;
 import com.mongodb.*;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.result.UpdateResult;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.slf4j.Logger;
@@ -22,6 +26,7 @@ import java.util.List;
 
 import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Updates.set;
 
 @Named("userService")
 public class UserServiceImpl implements UserService {
@@ -51,14 +56,19 @@ public class UserServiceImpl implements UserService {
     public Response authorization (User user){
         if(user.getLogin() != null || user.getPassword() != null) {
             logger.info("User authorization start: " + user.getLogin());
+            if(user.getLogin() == null || user.getLogin() == "") return new Response(false, "Логин недопустим");
+            if(user.getPassword() == null || user.getPassword() == "") return new Response(false, "Пароль недопустим");
             String login = user.getLogin();
+            String password = user.getPassword();
             MongoDatabase database = new MongoDbUtill().getDataBase();
             MongoCollection<Document> collection =  database.getCollection("Accounts");
-            FindIterable<Document> userList = collection.find(eq("accounts.abiturients."+login+".accountinf.login", login));
-            for(Document doc: userList){
-                if(doc.get("password") == user.getPassword()){
-                    return new Response(true, doc.toJson());
-                }
+            FindIterable<Document> userList =
+                    collection.find(and(
+                            eq("accounts.abiturients."+login+".accountinf.login", login),
+                            eq("accounts.abiturients."+login+".accountinf.password", password)));
+            if(userList.iterator().hasNext()) {
+                String json = userList.iterator().next().toJson();
+                return new Response(true, (Object) json);
             }
         }
         return new Response(false, "Неверно указан логин или пароль");
@@ -66,27 +76,36 @@ public class UserServiceImpl implements UserService {
 
     public Response registration(UserRegistration user){
         logger.info("Start user registration with login:"+user.getLogin());
-        if(user.getLogin() == null || user.getLogin() == "") return new Response(false, "Логин недопустим");
-        if(user.getPassword() == null || user.getPassword() == "") return new Response(false, "Пароль недопустим");
+        String login = user.getLogin();
+        String password = user.getPassword();
+        String controlAnswer = user.getControlAnswer();
+        String controlQuestion = user.getControlQuestion();
+        String firstName = user.getFirstName();
+        String lastName = user.getLastName();
+        String petronimic = user.getPatronymic();
+        if(login == null || login == "") return new Response(false, "Логин недопустим");
+        if(password == null || password == "") return new Response(false, "Пароль недопустим");
         if(user.getControlAnswer() == null || user.getControlAnswer() == "") return new Response(false, "Контрольный ответ недопустим");
         if(user.getControlQuestion() == null || user.getControlQuestion() == "") return new Response(false, "Контрольный вопрос недопустим");
         if(user.getFirstName() == null || user.getFirstName() == "") return new Response(false, "Имя недопустимо");
         if(user.getLastName() == null || user.getLastName() == "") return new Response(false, "Фамилия недопустима");
 
-        String login = user.getLogin();
         MongoDatabase database = new MongoDbUtill().getDataBase();
         MongoCollection<Document> collection =  database.getCollection("Accounts");
-        FindIterable<Document> findIt = collection.find(eq("accounts.abiturients."+login+".accountinf.login", login));
+        FindIterable<Document> findIt = collection.find(eq(login+".accountinf.login", login));
         Iterator iterator = findIt.iterator();
         if(!iterator.hasNext()) {
-            logger.info("registration user:" + login);
-            // BasicDBObject document = new BasicDBObject();
-            //document.put("name", "Shubham");
-            //document.put("company", "Baeldung");
-            //.insert(document);
+            BasicDBObject accountInfo = new BasicDBObject();
+            accountInfo.put("login", login);
+            accountInfo.put("password", password);
+            accountInfo.put("secretquestion", controlQuestion);
+            accountInfo.put("secretanswer", controlAnswer);
+            UpdateResult result = collection.updateOne(new BasicDBObject(), set(login,new BasicDBObject("accountinf",accountInfo)));
+            logger.info("Registration user with info:" + result.toString());
+
             return new Response(true, (Object) "Пользователь успешно зарегистрирован");
         }
-
+        logger.info("User is already exist with login: " + login);
         return new Response(false, "Указанный логин уже существует");
     }
 
@@ -105,7 +124,7 @@ public class UserServiceImpl implements UserService {
                 return new Response(true, (Object) "OK");
             }
         }
-        return new Response((false, "Логин не существует или контрольный ответ неверный");
+        return new Response(false, "Логин не существует или контрольный ответ неверный");
     }
 
     public User getUserById(Integer userId) {
